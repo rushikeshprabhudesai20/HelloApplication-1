45de32a620c5ba0267379903e69e8f530e3b8a68
Rushikesh-Prabhudesai <rushikeshprabhudesai20@gmail.com>
2019-08-22 12:23:36 +0530

    rushikesh_p

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java -- We are Awesome !!!!");
         System.out.println("I am enjoying the training...");
-
+        System.out.println("this is rushikesh.");
         //this is a comment from Rushikesh Prabhudesai.
     }
 }
